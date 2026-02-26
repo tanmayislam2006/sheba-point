e9655be8c1304c8f766ed8c60ca8f575cec8ddb8
tanmay2006 <tanmayislam101@gmail.com>
2026-02-26 13:15:18 +0600

    feat: implement authentication module with user, session, and account models

--- server/src/app/routes/index.ts
+++ server/src/app/routes/index.ts
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import { authRoutes } from "../module/auth/auth.routes";
 
 const router=Router()
+
+router.use('/auth',authRoutes)
 export const IndexRoutes = router;
