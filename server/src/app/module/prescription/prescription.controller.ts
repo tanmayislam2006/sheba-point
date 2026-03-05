a84e2ac895094c38b33d2ceaf6030c87f8539b9f
tanmay2006 <tanmayislam101@gmail.com>
2026-03-05 12:25:43 +0600

    feat: implement prescription management with CRUD operations, PDF generation, and email notifications

---
+++ server/src/app/module/prescription/prescription.controller.ts
@@ -0,0 +1,77 @@
+import { Request, Response } from "express";
+import httpStatus from "http-status";
+import { prescriptionService } from "./prescription.service";
+import catchAsync from "../../shared/asyncHandler";
+import { IRequestUser } from "../admin/admin.interface";
+import sendResponse from "../../shared/sendResponse";
+
+const givePrescription = catchAsync(async (req, res) => {
+  const payload = req.body;
+  const user = req.user as IRequestUser;
+  const result = await prescriptionService.givePrescription(user, payload);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Prescription created successfully",
+    data: result,
+  });
+});
+
+const myPrescriptions = catchAsync(async (req, res) => {
+  const user = req.user as IRequestUser;
+  const result = await prescriptionService.myPrescriptions(user);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Prescription fetched successfully",
+    data: result,
+  });
+});
+
+const getAllPrescriptions = catchAsync(async (req, res) => {
+  const result = await prescriptionService.getAllPrescriptions();
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Prescriptions retrieval successfully",
+    data: result,
+  });
+});
+
+const updatePrescription = catchAsync(async (req, res) => {
+  const user = req.user as IRequestUser;
+  const prescriptionId = req.params.id;
+  const payload = req.body;
+  const result = await prescriptionService.updatePrescription(
+    user,
+    prescriptionId as string,
+    payload,
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Prescription updated successfully",
+    data: result,
+  });
+});
+
+const deletePrescription = catchAsync(async (req, res) => {
+  const user = req.user as IRequestUser;
+  const prescriptionId = req.params.id;
+  await prescriptionService.deletePrescription(user, prescriptionId as string);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Prescription deleted successfully",
+  });
+});
+
+export const prescriptionController = {
+  givePrescription,
+  myPrescriptions,
+  getAllPrescriptions,
+  updatePrescription,
+  deletePrescription,
+};
---
+++ server/src/app/module/prescription/prescription.routes.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import { Role } from "../../../generated/prisma/enums";
+import { authGaud } from "../../middleware/authGaud";
+import { validateRequest } from "../../middleware/validateRequest";
+import { prescriptionController } from "./prescription.controller";
+import { prescriptionValidation } from "./prescription.validation";
+const router = express.Router();
+
+router.get(
+  "/",
+  authGaud(Role.SUPER_ADMIN, Role.ADMIN),
+  prescriptionController.getAllPrescriptions,
+);
+
+router.get(
+  "/my-prescriptions",
+  authGaud(Role.PATIENT, Role.DOCTOR),
+  prescriptionController.myPrescriptions,
+);
+
+router.post(
+  "/",
+  authGaud(Role.DOCTOR),
+  validateRequest(prescriptionValidation.createPrescriptionZodSchema),
+  prescriptionController.givePrescription,
+);
+
+router.patch(
+  "/:id",
+  authGaud(Role.DOCTOR),
+  validateRequest(prescriptionValidation.updatePrescriptionZodSchema),
+  prescriptionController.updatePrescription,
+);
+
+router.delete(
+  "/:id",
+  authGaud(Role.DOCTOR),
+  prescriptionController.deletePrescription,
+);
+
+export const PrescriptionRoutes = router;
