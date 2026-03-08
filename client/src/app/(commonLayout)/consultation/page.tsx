8b4780ef18e9759b99a23a524bd841b8e3e6cf77
tanmay2006 <tanmayislam101@gmail.com>
2026-03-08 10:12:41 +0600

    added the necessary route

---
+++ client/src/app/(commonLayout)/consultation/page.tsx
@@ -0,0 +1,13 @@
+
+
+const ConsultationPage = async () => {
+
+ return (
+   // Neat! Serialization is now as easy as passing props.
+   // HydrationBoundary is a Client Component, so hydration will happen there.
+
+   <h1 className="">ConsultationPage</h1>
+ );
+}
+
+export default ConsultationPage
---
+++ client/src/app/not-found.tsx
@@ -0,0 +1,3 @@
+export default function NotFoundPages() {
+    return <h1 className="text-4xl text-center">Global Page Not found</h1>
+};
