30495fed43c81846eca36e194770fd63087dcf20
tanmay2006 <tanmayislam101@gmail.com>
2026-03-08 09:50:02 +0600

    setting upt the env and make the project init

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,8 @@
+import { env } from "@/env";
 import Image from "next/image";
 
 export default function Home() {
+  const url = env.NEXT_PUBLIC_URL
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -14,8 +16,9 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            {url}
           </h1>
+
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
